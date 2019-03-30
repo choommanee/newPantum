3051e0f4eff52efc5ff3f9e60e3eafb7ba9384dc
choommanee <choommanee.sakda@gmail.com>
2019-03-30 15:45:34 +0700

    tes

--- Themes/Storefront/routes/public.php
+++ Themes/Storefront/routes/public.php
@@ -1,4 +1,4 @@
 <?php
 
 Route::get('products/{slug}/quick-view', 'ProductQuickViewController@show')->name('products.quick_view.show');
-Route::get('clear/route', 'InstallController@clearRoute');
+Route::get('clear/route', 'ProductQuickViewController@clearRoute');
