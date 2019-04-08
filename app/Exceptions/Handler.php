3d2e37c365bb7385306e5dbf762154b005504b1a
choommanee <choommanee.sakda@gmail.com>
2019-04-08 10:53:30 +0700

    dd
    vv

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,7 +42,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        var_dump($e);
+        dd($e);
         if ($e instanceof Swift_TransportException) {
             return back()->withInput()
                 ->with('error', trans('core::messages.mail_is_not_configured'));
