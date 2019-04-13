59afac855ffb2581523c6ac46e96686915baa41a
choommanee <choommanee.sakda@gmail.com>
2019-04-13 20:17:34 +0700

    ss
    sss

--- Themes/Storefront/views/public/products/partials/product/tab_contents/additional_information.blade.php
+++ Themes/Storefront/views/public/products/partials/product/tab_contents/additional_information.blade.php
@@ -1,16 +1,15 @@
 <div id="additional-information" class="specification tab-pane fade in" style="background-color: #f6f6f6;">
     <div class="table-responsive" style="margin-bottom: 20px;">
 
-        <center>
+       
             <h1>Specifications</h1>
             <table class="table" style="width: 90%;">
             <tbody>
             @foreach ($product->attributeSets as $attributeSet => $attributes)
                 <tr>
-                    <td colspan="2" style="border-bottom: 2px solid #ff000d;"><h4>{{ $attributeSet }}</h4></td>
+                    <td style="border-bottom: 2px solid #ff000d;"><h4>{{ $attributeSet }}</h4></td>
                 </tr>
                 <tr>
-                    <td width="20px"> </td>
                     <td style="">
                         @foreach ($attributes as $attribute)
                             <div class="information-data clearfix">
