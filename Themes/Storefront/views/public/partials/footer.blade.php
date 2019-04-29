0ebe08947ae03fb4397478056bc8cf7ab4a5b067
choommanee <choommanee.sakda@gmail.com>
2019-04-29 07:39:57 +0700

    dd
    dd

--- Themes/Storefront/views/public/partials/footer.blade.php
+++ Themes/Storefront/views/public/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 <footer class="footer">
-    <div class="container-fluid">
+    <div class="container2">
         <div class="footer-top p-tb-50 clearfix">
             <div class="row">
                 <div class="col-md-3">
